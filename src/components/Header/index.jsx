e8e273c0e4232b13ff061893bd331b8a38b08b91
Obande Otse <otseobande@gmail.com>
2018-11-27 19:46:17 +0100

    feat(dimensions): implement board dimension dialog
    [Delivers #162236449]

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="app-header">
+    <h1>Maze Problem</h1>
+  </header>
+);
+
+export default Header;
